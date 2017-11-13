1d6b66569e4fdc3cf4a95fea65cbc57e56c209cb
connolly852 <32278743+connolly852@users.noreply.github.com>
2017-11-13 15:03:28 -0500

    Add files via upload

---
+++ Phase3.R
@@ -0,0 +1,9 @@
+library(dplyr)
+
+dont_crash <- read.csv(file = '~/Desktop/lending-club-loan-data/loan.csv')
+
+loan_tidy <- dont_crash %>%
+  select(acc_now_delinq, annual_inc, annual_inc_joint, delinq_2yrs, emp_length, emp_title, 
+          grade, int_rate, issue_d,  loan_amnt, loan_status, open_acc, open_il_24m, purpose, 
+         term, total_rec_late_fee, zip_code)
+
